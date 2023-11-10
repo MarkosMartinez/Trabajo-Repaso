0f1cb077c05a834003520b27b33f789d4b4c1bfb
Markos <ikbdk@plaiaundi.net>
2023-11-10 11:43:03 +0000

    Ultimo logueo en añadido y zona horaria actualizad

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -26,6 +27,8 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
+
+        DB::table('users')->where('id', Auth::user()->id)->update(['last_login' => now()]);
 
         $request->session()->regenerate();
 
