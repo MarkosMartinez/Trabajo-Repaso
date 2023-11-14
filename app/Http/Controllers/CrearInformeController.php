87922718f513a871c0a4f09a886fb55adeaad8ac
Markos <ikbdk@plaiaundi.net>
2023-11-14 11:26:53 +0000

    controlador del form creado

--- app/Http/Controllers/CrearInformeController.php
+++ app/Http/Controllers/CrearInformeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Formulario;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -21,7 +22,33 @@ class CrearInformeController extends Controller
 
     public function procesarForm(Request $request)
 {
-    $user = $request->user();
-    $user = User::find($user->id);
+
+    // Validar los datos del formulario
+    $request->validate([
+        'nombre' => 'required|string',
+        'asunto' => 'required|string',
+        'contenido' => 'required|string',
+    ]);
+
+    // Opcion 1:
+    // Crear y almacenar el nuevo formulario
+    // $formulario = new Formulario();
+    // $formulario->nombre = $request->input('nombre');
+    // $formulario->asunto = $request->input('asunto');
+    // $formulario->contenido = $request->input('contenido');
+    // $formulario->user_id = auth()->user()->id; // Asignar el ID del usuario autenticado como foreign key
+    // $formulario->save();
+
+    // Opcion 2:
+    // Crear y almacenar el nuevo formulario utilizando el método create
+    $formulario = Formulario::create([
+        'nombre' => $request->input('nombre'),
+        'asunto' => $request->input('asunto'),
+        'contenido' => $request->input('contenido'),
+        'user_id' => auth()->user()->id, // Asignar el ID del usuario autenticado como foreign key
+    ]);
+
+    // Redireccionar a una página de éxito o mostrar un mensaje de confirmación
+    return redirect("/dashboard")->with('success', 'Formulario enviado correctamente');
 }
 }
